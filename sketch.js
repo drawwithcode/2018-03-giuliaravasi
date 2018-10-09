641475a48e9c06d0ef97bbb1fba3548911c9fd64
Giulia <giuliaravasi@MacBook-Pro-di-Giulia.local>
2018-10-09 10:49:42 +0200

    Assignment_3

--- sketch.js
+++ sketch.js
@@ -2,10 +2,89 @@ function preload(){
   // put preload code here
 }
 
+var myColors = ['salmon', 'darkorchid', 'crimson', 'darkorange',
+'gold', 'lightgreen', 'paleturquoise', 'dodgerblue', 'rosybrown',
+'maroon', 'Aquamarine', 'SpringGreen', 'Tomato', 'LightCoral',
+'MediumPurple', 'Pink']
+
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+  background('#e6ffff');
+  frameRate(5)
+
 }
 
 function draw() {
-  // put drawing code here
+  for(var x = 30; x < width; x += 130){
+
+  for(var y = 30; y < height; y += 130) {
+
+    var r = random(0, myColors.length - 1);
+    r = Math.round(r);
+
+    if(random()<0.8) {
+      fill(myColors[r]);
+      noStroke();
+      ellipse(x, y, 40);
+    } else {
+      fill(200);
+      noStroke();
+      ellipse(x, y, 40);
+    }
+    }
+  }
+  for(var x = 95; x < width; x += 130){
+
+  for(var y = 30; y < height; y += 130) {
+
+    var r = random(0, myColors.length - 1);
+    r = Math.round(r);
+
+    if(random()<0.8) {
+      fill(myColors[r]);
+      noStroke();
+      ellipse(x, y, 30);
+    } else {
+      fill(200);
+      noStroke();
+      ellipse(x, y, 30);
+    }
+    }
+  }
+  for(var x = 30; x < width; x += 130){
+
+  for(var y = 95; y < height; y += 130) {
+
+    var r = random(0, myColors.length - 1);
+    r = Math.round(r);
+
+    if(random()<0.8) {
+      fill(myColors[r]);
+      noStroke();
+      ellipse(x, y, 20);
+    } else {
+      fill(200);
+      noStroke();
+      ellipse(x, y, 20);
+    }
+    }
+  }
+  for(var x = 95; x < width; x += 130){
+
+  for(var y = 95; y < height; y += 130) {
+
+    var r = random(0, myColors.length - 1);
+    r = Math.round(r);
+
+    if(random()<0.8) {
+      fill(myColors[r]);
+      noStroke();
+      ellipse(x, y, 50);
+    } else {
+      fill(180);
+      noStroke();
+      ellipse(x, y, 50);
+    }
+    }
+  }
 }
